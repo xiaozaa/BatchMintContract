16e860c25f35f13496ef94e65bbadb34daa63221
anzhang <zhangan@anzhangs-MacBook-Pro.local>
2022-03-31 20:34:21 -0700

    delete unused parameter

--- batchMint.sol
+++ batchMint.sol
@@ -7,47 +7,47 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "./ERC721A.sol";
 
 contract Deployed is ReentrancyGuard {
-    
     function saleMint(uint256 _ammount) external payable nonReentrant {}
-    
+
     function revealed() public pure returns (bool) {}
 
-    function priceSale() public pure returns (uint) {}
-    
+    function priceSale() public pure returns (uint256) {}
 }
 
-contract contractMint is ReentrancyGuard , IERC721Receiver, Ownable{
+contract contractMint is ReentrancyGuard, IERC721Receiver, Ownable {
     Deployed dc;
-    address parser;
-    
-    function onERC721Received(address _operator, address _from, uint256 _tokenId, bytes memory _data) public override returns(bytes4) {
-       return 0x150b7a02;
+
+    function onERC721Received(
+        address _operator,
+        address _from,
+        uint256 _tokenId,
+        bytes memory _data
+    ) public override returns (bytes4) {
+        return 0x150b7a02;
     }
 
-    function existing(address _t) external onlyOwner{
+    function existing(address _t) external onlyOwner {
         dc = Deployed(_t);
     }
 
     // TODO: renounceOwnership
- 
-    function getStatus() public view returns (bool result){
+
+    function getStatus() public view returns (bool result) {
         return dc.revealed();
     }
 
-    function getPrice() public view returns (uint result){
+    function getPrice() public view returns (uint256 result) {
         return dc.priceSale();
     }
 
-    function setParser(address _t) external onlyOwner {
-        parser = _t;
-    }
-    
     function mint(uint256 _val) external payable onlyOwner nonReentrant {
         dc.saleMint{value: _val * getPrice()}(_val);
     }
 
-    function mint(address _e, uint256 _val) external payable onlyOwner{
-        (bool success, ) = _e.call{value: _val * getPrice()}(abi.encodePacked(bytes4(keccak256("saleMint(uint256)")), _val)); // D's storage is set, E is not modified 
+    function mint(address _e, uint256 _val) external payable onlyOwner {
+        (bool success, ) = _e.call{value: _val * getPrice()}(
+            abi.encodePacked(bytes4(keccak256("saleMint(uint256)")), _val)
+        ); // D's storage is set, E is not modified
         require(success);
     }
 
@@ -56,7 +56,11 @@ contract contractMint is ReentrancyGuard , IERC721Receiver, Ownable{
         require(success, "Transfer failed.");
     }
 
-    function withdrawNFT(address _e, uint256 id) external nonReentrant onlyOwner {
+    function withdrawNFT(address _e, uint256 id)
+        external
+        nonReentrant
+        onlyOwner
+    {
         ERC721A token = ERC721A(_e);
         require(token.balanceOf(address(this)) > 0, "Caller must own nft");
         require(token.ownerOf(id) == address(this), "You must own the token");
